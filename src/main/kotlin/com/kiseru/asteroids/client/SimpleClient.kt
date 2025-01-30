ab84dff6238f8e620fbca300ea20489a4256fed5
Alexandr Kiselev <kis.alex97@gmail.com>
2025-01-30 12:44:10 +0300

    added support for waiting for connection to the server

--- src/main/kotlin/com/kiseru/asteroids/client/SimpleClient.kt
+++ src/main/kotlin/com/kiseru/asteroids/client/SimpleClient.kt
@@ -2,8 +2,10 @@ package com.kiseru.asteroids.client
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.net.ConnectException
 import java.net.Socket
 
 private const val PORT = 6501
@@ -11,7 +13,7 @@ private const val HOST = "localhost"
 
 suspend fun main(): Unit =
     coroutineScope {
-        val socket = withContext(Dispatchers.IO) { Socket(HOST, PORT) }
+        val socket = connectToServer()
         launch(Dispatchers.IO) {
             val listener = ServerListener(socket.getInputStream(), System.out)
             listener.listen()
@@ -22,3 +24,15 @@ suspend fun main(): Unit =
             client.startClient()
         }
     }
+
+private suspend fun connectToServer(): Socket {
+    while (true) {
+        try {
+            println("Trying to connect to server...")
+            return withContext(Dispatchers.IO) { Socket(HOST, PORT) }
+        } catch (e: ConnectException) {
+            println("Failed to connect to server. Next attempt in 10 seconds.")
+            delay(10000)
+        }
+    }
+}
